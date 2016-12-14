64009e75a7d15967d15df6cf5042e23907c9a846
Anton Zaec <antonrumato@yandex.ru>
2016-12-14 18:00:42 +0300

    Functions for numbers printing

---
+++ src/print_num.erl
@@ -0,0 +1,20 @@
+-module(print_num).
+-export([print_all/1, print_odd/1]).
+
+print_range(I, Bound) when I =< Bound ->
+	io:format('~B~n', [I]),
+	print_range(I + 1, Bound);
+print_range(_, _) -> true.
+
+print_all(N) -> 
+	print_range(1, N).
+
+print_odd_range(I, Bound) when I =< Bound -> 
+	case (I rem 2) =/= 0 of
+		true -> io:format('~B~n', [I]);
+		_ -> true
+	end,
+	print_odd_range(I + 1, Bound);
+print_odd_range(_, _) -> true.
+print_odd(N) ->
+	print_odd_range(1, N).

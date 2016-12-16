9f30d86ca4a5a1d78fa01d8771fc05e774e28ece
Anton Zaec <antonrumato@yandex.ru>
2016-12-16 19:04:08 +0300

    First version of function for parsing arithmetic expressions.

---
+++ src/calc.erl
@@ -0,0 +1,75 @@
+-module(calc).
+-export([parse/1]).
+
+parse(String) ->
+	Tokens = tokenize(String),
+	{Result, _} = parse_impl(Tokens, {}),
+	Result.
+
+tokenize([]) ->
+	[];
+tokenize([H|T]) ->
+	State = if 
+			H >= $0 andalso H =< $9 -> read_number;
+			true -> read_operator
+		end,
+	String = [H|T],
+	case State of
+		read_operator -> [{operator, get_operator_name(H)}|tokenize(T)];
+		read_number -> Number = list_to_integer(extrude_number(String)),
+						StringWithoutNumber = remove_number(String),
+						[{number, Number}|tokenize(StringWithoutNumber)]
+	end.
+
+parse_impl([], Result) -> {Result, []};
+parse_impl(Tokens, Result) ->
+	{NewResult, NewNextTokens, MustContinue} = case Tokens of
+		[{operator, unary_minus},{operator, open_bracket}|T] -> 
+			{SubResult, NextTokens} = parse_impl(T, {}),
+			{{unary_minus, SubResult}, NextTokens, true};
+		[{operator, open_bracket}|T] -> 
+			{SubResult, NextTokens} = parse_impl(T, {}),
+			{SubResult, NextTokens, true};
+		[{_, close_bracket}|T] -> {Result, T, true};
+		[{number, Operand1}, {operator, Operator}, {number, Operand2}|T] -> 
+			SubResult = {Operator, {number, Operand1}, {number, Operand2}},
+			{SubResult, T, false};
+		[{number, Operand1}, {operator, Operator}, {operator, open_bracket}|T] -> 
+			NextBracket = [{operator, open_bracket}|T],
+			{NextBracketResult, NextTokens} = parse_impl(NextBracket, {}),
+			SubResult = {Operator, {number, Operand1}, NextBracketResult},
+			{SubResult, NextTokens, false};
+		[{operator, Operator}, {number, Operand2}|T] -> 
+			SubResult = {Operator, Result, {number, Operand2}},
+			{SubResult, T, false};
+		[{operator, Operator}, {operator, open_bracket}|T] ->
+			NextBracket = [{operator, open_bracket}|T],
+			{RightSubResult, NextTokens} = parse_impl(NextBracket, {}),
+			SubResult = {Operator, Result, RightSubResult},
+			{SubResult, NextTokens, false}
+	end,
+	case MustContinue of
+		true -> parse_impl(NewNextTokens, NewResult);
+		false -> {NewResult, NewNextTokens}
+	end.
+
+get_operator_name(OpChar) ->
+	case OpChar of
+		$+ -> plus;
+		$~ -> unary_minus;
+		$- -> minus;
+		$* -> multiply;
+		$/ -> division;
+		$( -> open_bracket;
+		$) -> close_bracket
+	end.	
+
+extrude_number([H|T]) when H >= $0 andalso H =< $9 ->
+	[H|extrude_number(T)];
+extrude_number(_) ->
+	[].
+
+remove_number([H|T]) when H >= $0 andalso H =< $9 ->
+	remove_number(T);
+remove_number(L) ->
+	L.
